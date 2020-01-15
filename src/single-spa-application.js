c2a81f7cd953da3ea2893b02ca89df2f19e07bd6
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-01-15 19:48:31 +0100

    Inclusión de bootstrap-vue en script de single-spa

--- src/single-spa-application.js
+++ src/single-spa-application.js
@@ -1,8 +1,14 @@
 import "./set-public-path";
 import Vue from "vue";
 import singleSpaVue from "single-spa-vue";
-
 import App from "./App.vue";
+
+import { BootstrapVue } from "bootstrap-vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
