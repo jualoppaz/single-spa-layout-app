ff2faa8641e8cf9dab0efb2e673545d7c991fbf4
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-01-23 21:39:05 +0100

    Adds font awesome library to main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue';
 
-import { BootstrapVue } from "bootstrap-vue";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import { BootstrapVue } from 'bootstrap-vue';
+import App from './App.vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+library.add(faGithub);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 // Install BootstrapVue
 Vue.use(BootstrapVue);
