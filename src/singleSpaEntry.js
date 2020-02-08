0b395f63fd56b05e6c783778567ae866707bbf59
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-02-09 00:32:08 +0100

    Import faHome icon in main scripts

--- src/singleSpaEntry.js
+++ src/singleSpaEntry.js
@@ -4,13 +4,16 @@ import singleSpaVue from 'single-spa-vue';
 import { BootstrapVue } from 'bootstrap-vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import App from './App.vue';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 library.add(faGithub);
+library.add(faHome);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
